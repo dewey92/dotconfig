b876d3aa942cba6cbb1b4b90697f03d74653ea1b
Jihad D. Waspada <dewey992@gmail.com>
2020-11-18 17:35:00 +0100

    Add rofi config

--- fish/config.fish
+++ fish/config.fish
@@ -3,6 +3,8 @@ source $HOME/.config/fish/aliases/chordify.fish
 
 # neofetch --disable theme icons term_font
 
+set -x XDG_DATA_DIRS "/usr/local/share:/usr/share"
+
 set -x FD_OPTIONS "--follow --exclude .git --exclude node_modules"
 set -x FZF_DEFAULT_OPTS "--layout=reverse --preview \"bat --style=numbers --color=always {}\""
 set -x FZF_DEFAULT_COMMAND "fd --hidden $FD_OPTIONS"
