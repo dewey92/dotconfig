930950f588d977714b9ddc87ff888635b536382c
Jihad D. Waspada <dewey992@gmail.com>
2021-09-24 17:25:27 +0200

    feat(nvim): add syntax highlighting for styled components + vimscript inside lua

---
+++ nvim/after/queries/ecma/injections.scm
@@ -0,0 +1,5 @@
+(call_expression
+  function: (member_expression
+              (identifier) @_styled
+              (#match? @_styled "^(linaria|styled)$"))
+  arguments: (template_string) @css)
---
+++ nvim/after/queries/lua/injections.scm
@@ -0,0 +1,10 @@
+(
+  (function_call
+    (field_expression
+      (property_identifier) @_nvim_exec_method)
+    (arguments
+      (string) @vim)
+  )
+
+  (#eq? @_nvim_exec_method "nvim_exec")
+)
