fcd788b6f249bbfbc5da17502bb3faf069532ce9
Jihad D. Waspada <dewey992@gmail.com>
2026-02-26 15:19:27 +0700

    feat: update essential

--- install_essential.sh
+++ install_essential.sh
@@ -1,7 +1,28 @@
-# Install essentials
-yay -S kitty neovim xsel brave bat lazygit git-delta-bin fd ripgrep fzf ranger \
-	picom sxhkd bspwm polybar rofi siji \
-	zsh bpytop exa flmeshot pandoc playerctl && \
+installApps () {
+  # Install essentials
+  printf "Installing essentials..."
+  yay -S kitty neovim xsel brave bat lazygit git-delta-bin fd ripgrep fzf ranger keyd \
+    picom sxhkd bspwm polybar rofi siji \
+    zsh bpytop exa flmeshot pandoc playerctl;
 
-# Install Node through NVM
-wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.0/install.sh | bash
+  # Install Node through NVM
+  printf "Installing NPM..."
+  wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.0/install.sh | bash;
+}
+
+setupKeyd () {
+  ln -s ~/.config/keyd/default.conf /etc/keyd/default.conf;
+  # Enable unicode support in the config file
+  ln -s /usr/share/keyd/keyd.compose ~/.XCompose;
+}
+
+# @see https://askubuntu.com/a/623968
+spinner() {
+  local i sp n
+  sp='/-\|'
+  n=${#sp}
+  printf ' '
+  while sleep 0.1; do
+    printf "%s\b" "${sp:i++%n:1}"
+  done
+}
